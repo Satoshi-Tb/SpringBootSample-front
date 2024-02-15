e435bfbaed14d896af3678a3c6fea213fef1d821
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-02-16 00:46:11 +0900

    ダミーログインapi追加

--- src/components/model/auth/Login/Login.tsx
+++ src/components/model/auth/Login/Login.tsx
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { SimpleLayout } from "../../layout";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import envConfig from "@/utils/envConfig";
 
 export const Login = () => {
+  const [userId, setUserId] = useState("");
+  const [passowrd, setPassword] = useState("");
+
+  const login = async () => {
+    const input: RequestInfo | URL = `${envConfig.apiUrl}/api/auth/login`;
+    try {
+      const resp = await fetch(input);
+      console.log("response", resp);
+      window.location.href = "/top";
+    } catch (e) {
+      console.error("Request failed", e);
+    }
+  };
+
   return (
     <Box
       sx={{ marginTop: 2 }}
       component="form"
       onSubmit={() => {
-        alert("ボタン押下");
+        alert(`ボタン押下 userId:${userId} password:${passowrd}`);
       }}
     >
       <Grid container spacing={2}>
-        <Grid item md={4}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignContent="center"
+        >
           <Typography>ユーザID</Typography>
-        </Grid>
-        <Grid item md={8}></Grid>
-        <Grid item md={4}>
+          <TextField
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={(event) => {
+              event.preventDefault();
+              setUserId(event.target.value);
+            }}
+          ></TextField>
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignContent="center"
+        >
           <Typography>パスワード</Typography>
-        </Grid>
-        <Grid item md={8}></Grid>
-        <Grid item xs={4}>
-          <Typography>ユーザー名</Typography>
-        </Grid>
-        <Grid item xs={8}></Grid>
+          <TextField
+            placeholder="パスワード"
+            value={passowrd}
+            onChange={(event) => {
+              event.preventDefault();
+              setPassword(event.target.value);
+            }}
+          ></TextField>
+        </Box>
         <Grid item xs={12}>
           <Box display="flex" flexDirection="row" justifyContent="center">
             <Button variant="contained" sx={{ marginRight: 1 }} type="submit">
