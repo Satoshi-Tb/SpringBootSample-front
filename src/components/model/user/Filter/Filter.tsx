7fb0bb58b37f189e81d16fd05fd0dcad9b74cc4d
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-04-07 23:25:27 +0900

    不要処理の削除

--- src/components/model/user/Filter/Filter.tsx
+++ src/components/model/user/Filter/Filter.tsx
@@ -1,33 +1,10 @@
-import { useListSearchConditionState } from "@/components/store/useListSearchConditionState";
-import {
-  useSelectedFilterListMutator,
-  useSelectedFilterListState,
-} from "@/components/store/useSelectedFilterListState";
-import { useUserFilter } from "@/components/usecase/useUserFilter";
 import { Typography } from "@mui/material";
 import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
 import React, { useState } from "react";
 import { FilterItem } from "../FilterItem/FilterItem";
 
 const Filter = () => {
-  // 再読込処理用
-  const condition = useListSearchConditionState();
-
   const [lastSelectedItem, setLastSelectedItem] = useState<string | null>(null);
-  const selectedFilterList = useSelectedFilterListState();
-  const { setSelectedFilterList } = useSelectedFilterListMutator();
-
-  const {
-    userFilterData: depFilterData,
-    hasError: depHasError,
-    isLoading: depLoading,
-  } = useUserFilter("department", condition);
-
-  const { userFilterData, hasError, isLoading } = useUserFilter(
-    "gender",
-    condition
-  );
-  console.log("userFilterData", userFilterData);
 
   const handleItemSelectionToggle = (
     event: React.SyntheticEvent,
